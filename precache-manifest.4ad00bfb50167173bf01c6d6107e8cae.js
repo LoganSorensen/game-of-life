319fae41251bb44ee688fc4bba6df0f0913f7ea4
Logan Sorensen <loganmsorensen@gmail.com>
2020-05-28 16:29:48 -0700

    Updates

---
+++ precache-manifest.4ad00bfb50167173bf01c6d6107e8cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c77c980d2dc4036df4232119f04ba54",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "4db45e992399ceec384d",
+    "url": "/game-of-life/static/css/main.1a4a17ee.chunk.css"
+  },
+  {
+    "revision": "5fe333e0925f7feab942",
+    "url": "/game-of-life/static/js/2.c5f828b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.c5f828b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db45e992399ceec384d",
+    "url": "/game-of-life/static/js/main.3e7e8968.chunk.js"
+  },
+  {
+    "revision": "74c0ec62b28c760df103",
+    "url": "/game-of-life/static/js/runtime-main.ccc8ab68.js"
+  },
+  {
+    "revision": "884e378dcddb5f0233ba84e6b7db49a6",
+    "url": "/game-of-life/static/media/index.884e378d.less"
+  }
+]);
